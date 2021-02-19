cb1e0b977ec5c5d97cfd351ad5736c0c7f8536e3
Jonathan Yates <carlock1609@gmail.com>
2021-02-19 09:15:58 -0800

    updating tutorials

---
+++ Section-18-ASYNC_ACTIONS_THUNK/blog/src/components/PostList.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+const PostList = (props) => {
+  
+    return (
+      <div>Post List</div>
+    );
+};
+
+export default PostList;
---
+++ Section-18-ASYNC_ACTIONS_THUNK/blog/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  // dummy key to temp void err
+  replaceMe: () => 'hi there'
+});
